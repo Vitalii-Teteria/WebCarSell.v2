b17275f671e58e86cf792c0ddef2fa50ed863e1c
vitalii_teteria <teterya.vitalya@gmail.com>
2022-03-27 17:27:12 +0300

    Init project

---
+++ WebCarSell.BusinessLogic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebCarSell.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebCarSell.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebCarSell.DataAccess
+{
+    public class Class1
+    {
+    }
+}
